afe96ea629b556a25dac3c876af79a80fa77c2b3
its-amogh-21 <auuchavan@gmail.com>
2022-05-11 19:49:44 +0530

    adding title logo to the code

---
+++ demo.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  7 13:36:47 2022
+
+@author: auuch
+"""
+
+import nltk
+
+sentence = "Food is good man gooddx1"
+print(nltk.word_tokenize(sentence))
